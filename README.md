0e1f79fe3d73dd8f4b34dfb9170bb5af8b2ef368
Baird Castleberry <baird@bairdcastleberry.com>
2014-09-01 02:40:36 -0500

    initial

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 moment
 ======
 
-Remember this moment
+Example project based on real-time feeds and open data to define a moment in time...
---
+++ client/scripts/main.js
@@ -0,0 +1,190 @@
+
+//// Write Date
+
+function writeDate() {
+    var date = new Date();
+    var element = document.createElement('div');
+    element.id = "date";
+    document.body.appendChild(element);
+    element.appendChild(document.createTextNode
+        (date.toDateString()));
+}
+
+//// Write Time
+
+function writeTime() {
+    var date = new Date();
+    var hours = date.getHours();    // Grabbing times in sections to create AM/PM format
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12;    // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    seconds = seconds < 10 ? '0'+seconds : seconds;
+    var time = hours + ':' + minutes + ':' + seconds + ' ' + ampm;
+    var element = document.createElement('div');
+    element.id = "time";
+    document.body.appendChild(element);
+    element.appendChild(document.createTextNode
+    	(time));
+}
+
+//// Get Headlines
+
+google.load("feeds", "1");
+
+    function writeNews() {
+      var feed = new google.feeds.Feed("https://news.google.com/?output=rss");
+      feed.setNumEntries(1)  // fetches 4 by default but this sets most recent headline
+      feed.load(function(result) {
+        if (!result.error) {
+          for (var i = 0; i < result.feed.entries.length; i++) {
+            var entry = result.feed.entries[i];
+            var element = document.createElement("div");
+            element.id = "news";
+            document.body.appendChild(element);
+            element.appendChild(document.createTextNode
+                (entry.title));
+            google.setOnLoadCallback(writeNews);  // put this inside function to write after click
+          }
+        }
+      });
+    }
+
+//// Get Moon Phase
+
+google.load("feeds", "1");
+
+    function writeMoon() {
+      var feed = new google.feeds.Feed("http://feeds.feedburner.com/FarmersAlmanac-CurrentMoonPhase");
+      feed.setNumEntries(1)  // fetches 4 by default but this sets most recent headline
+      feed.load(function(result) {
+        if (!result.error) {
+          for (var i = 0; i < result.feed.entries.length; i++) {
+            var entry = result.feed.entries[i];
+            var element = document.createElement("div");
+            element.id = "moon";
+            document.body.appendChild(element);
+            element.appendChild(document.createTextNode
+                (entry.title));
+            google.setOnLoadCallback(writeMoon);  // put this inside function to write after click
+          }
+        }
+      });
+    }
+
+//// Get EarthQuakes
+
+function writeQuake() {
+    $.ajax({
+        type: 'GET',
+        dataType: 'jsonp',
+        jsonp: false,
+        jsonpCallback: 'eqfeed_callback',
+        data: {},
+        url: "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojsonp",
+        success: function (msg) {
+            var quakeMag = msg["features"][0]["properties"]["mag"];
+            var quakePlace = msg["features"][0]["properties"]["place"];
+            var element = document.createElement('div');
+            element.id = "quake";
+            document.body.appendChild(element);
+            element.appendChild(document.createTextNode
+            ("Current Quake Location: " + (quakePlace) + " Magnitude: " + (quakeMag)));
+        }
+    });
+}
+
+//// BTC Value in USD
+
+function writeBTC() {
+    $.ajax({
+        type: 'GET',
+        dataType: 'jsonp',
+        data: {},
+        url: "http://coinabul.com/api.php",
+        success: function (msg) {
+            var number = msg["BTC"]["USD"];
+            var element = document.createElement('div');
+            element.id = "btc";
+            document.body.appendChild(element);
+            element.appendChild(document.createTextNode
+            ("Current value of Bitcoin in USD: " + Math.round(number)));
+        }
+    });
+}
+
+//// Gold Value in USD
+
+function writeGold() {
+    $.ajax({
+        type: 'GET',
+        dataType: 'jsonp',
+        data: {},
+        url: "http://coinabul.com/api.php",
+//        error: function (jqXHR, textStatus, errorThrown) {    // Example of error function
+//            console.log(jqXHR)
+//        },
+        success: function (msg) {
+            var number = msg["Gold"]["USD"];
+            var element = document.createElement('div');
+            element.id = "gold";
+            document.body.appendChild(element);
+            element.appendChild(document.createTextNode
+            ("Current value of Gold in USD: " + Math.round(number)));
+        }
+    });
+}
+
+//// Silver Value in USD
+
+function writeSilver() {
+    $.ajax({
+        type: 'GET',
+        dataType: 'jsonp',
+        data: {},
+        url: "http://coinabul.com/api.php",
+        success: function (msg) {
+            var number = msg["Silver"]["USD"];
+            var element = document.createElement('div');
+            element.id = "silver";
+            document.body.appendChild(element);
+            element.appendChild(document.createTextNode
+            ("Current value of Silver in USD: " + Math.round(number)));
+        }
+    });
+}
+
+//// Fade Remember Button
+
+//function fadeButton() {
+//    $(#moment).fadeOut(1000);
+//}
+
+//$(this).fadeOut(2000, function() {
+//        $(this).empty().show();
+//        });
+
+
+//////////////////////////////////////// REMEMBER THIS MOMENT!
+
+$(document).ready(function() {
+    $("#moment[data-action]").click(function(){
+        
+        
+        writeDate();
+        writeTime();
+        writeNews();
+        writeMoon();
+        writeQuake();
+        writeBTC();
+        writeGold();
+        writeSilver();
+        
+       // fadeButton();
+       // mixpanel.track("Moment Generated");
+
+    }); 
+});
+
